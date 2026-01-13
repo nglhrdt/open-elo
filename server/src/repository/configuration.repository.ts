fbd94ffa1118cd6dbabd6b9d59f04e64b7c53564
Simon Engelhardtm <smn@nglhrdt.de>
2026-01-13 20:17:24 +0000

    delete server components

--- server/src/repository/configuration.repository.ts
+++
@@ -1,6 +0,0 @@
-import { AppDataSource } from "../database/data-source";
-import { ConfigurationEntity } from "../database/entity/configuration.entity";
-
-const ConfigurationRepository = AppDataSource.getRepository(ConfigurationEntity);
-
-export default ConfigurationRepository;
